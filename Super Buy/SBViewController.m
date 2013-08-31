ba427d47c497f686465bdfda98327d040c03f249
Robert Witt <robert@roberts-imac.fritz.box>
2013-08-31 16:11:10 +0200

    Worklight API and necessary frameworks were added to the project. A sample call to Worklight server was successfully implemented in view controller.

--- Super Buy/SBViewController.m
+++ Super Buy/SBViewController.m
@@ -7,9 +7,31 @@
 //
 
 #import "SBViewController.h"
+#import "WorklightAPI/include/WLClient.h"
+#import "WorklightAPI/include/WLDelegate.h"
+#import "WorklightAPI/include/WLProcedureInvocationData.h"
 
 
-@interface SBViewController ()
+@interface MyInvocationListener : NSObject <WLDelegate>
+
+@end
+
+@implementation MyInvocationListener
+
+- (void)onSuccess:(WLResponse *)response
+{
+    NSLog(@"%@", [response getResponseJson]);
+}
+
+- (void)onFailure:(WLFailResponse *)response
+{
+    NSLog(@"%@", response.responseText);
+}
+
+@end
+
+
+@interface SBViewController () <WLDelegate>
 
 @end
 
@@ -20,12 +42,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[WLClient sharedInstance] wlConnectWithDelegate:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)onSuccess:(WLResponse *)response
+{
+    NSLog(@"%@", response.responseText);
+    
+    WLProcedureInvocationData *invocation = [[WLProcedureInvocationData alloc] initWithAdapterName:@"SOAPCRMMobileLoyalty" procedureName:@"getMembership"];
+    invocation.parameters = @[@"1"];
+    
+    MyInvocationListener *listener = [[MyInvocationListener alloc] init];
+    
+    [[WLClient sharedInstance] invokeProcedure:invocation withDelegate:listener];
+}
+
+- (void)onFailure:(WLFailResponse *)response
+{
+    NSLog(@"%@", response.responseText);
 }
 
 @end
